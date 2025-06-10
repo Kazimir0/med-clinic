8b59650543a5982fb9f2770df6456a86cc097f50
Catalin <Paraschiv16@yahoo.com>
2025-06-10 18:43:26 +0300

    Make the doctor dashboard

--- app/(protected)/doctor/page.tsx
+++ app/(protected)/doctor/page.tsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import { StatSummary } from "@/components/charts/stat-summary";
+import { getDoctorDashboardStats } from "@/utils/services/doctor";
+import { currentUser } from "@clerk/nextjs/server";
+import { BriefcaseBusiness, BriefcaseMedical, User, Users } from "lucide-react";
+import { AvailableDoctors } from "@/components/available-doctor";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { StatCard } from "@/components/stat-card";
+import { AppointmentChart } from "@/components/charts/appointment-chart";
+import { RecentAppointments } from "@/components/tables/recent-appointments";
 
-const DoctorDashboard = () => {
+const DoctorDashboard = async () => {
+  const user = await currentUser();
+  const {
+    totalPatient,
+    totalNurses,
+    totalAppointment,
+    appointmentCounts,
+    availableDoctors,
+    monthlyData,
+    last5Records,
+  } = await getDoctorDashboardStats();
+
+  const cardData = [
+    {
+      title: "Patients",
+      value: totalPatient,
+      icon: Users,
+      className: "bg-blue-600/15",
+      iconClassName: "bg-blue-600/25 text-blue-600",
+      note: "Total patients",
+      link: "/record/patients",
+    },
+    {
+      title: "Nurses",
+      value: totalNurses,
+      icon: User,
+      className: "bg-rose-600/15",
+      iconClassName: "bg-rose-600/25 text-rose-600",
+      note: "Total nurses",
+      link: "",
+    },
+    {
+      title: "Appointments",
+      value: totalAppointment,
+      icon: BriefcaseBusiness,
+      className: "bg-yellow-600/15",
+      iconClassName: "bg-yellow-600/25 text-yellow-600",
+      note: "Total appointments",
+      link: "/record/appointments",
+    },
+    {
+      title: "Consultation",
+      value: appointmentCounts?.COMPLETED,
+      icon: BriefcaseMedical,
+      className: "bg-emerald-600/15",
+      iconClassName: "bg-emerald-600/25 text-emerald-600",
+      note: "Total consultation",
+      link: "/record/appointments",
+    },
+  ];
+
   return (
-    <div>DoctorDashboard</div>
-  )
-}
+    <div className="rounded-xl py-6 px-3 flex flex-col xl:flex-row gap-6">
+      {/* LEFT */}
+      <div className=" w-full xl:w-[69%]">
+        <div className="bg-white rounded-xl p-4 mb-8">
+          <div className="flex items-center justify-between mb-6">
+            <h1 className="text-lg xl:text-2xl font-semibold">
+              Welcome, Doctor {user?.firstName} {user?.lastName}
+            </h1>
 
-export default DoctorDashboard
+            <Button size="sm" variant="outline" asChild>
+              <Link href={`/record/doctors/${user?.id}`}>View Profile</Link>
+            </Button>
+          </div>
+
+          <div className="w-full flex flex-wrap gap-2">
+            {cardData?.map((el, index) => (
+              <StatCard
+                key={index}
+                title={el?.title}
+                value={el?.value!}
+                icon={el.icon}
+                className={el.className}
+                iconClassName={el.iconClassName}
+                link={el.link}
+                note={el.note}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="h-[500px]">
+          <AppointmentChart data={monthlyData!} />
+        </div>
+
+        <div className="bg-white rounded-xl p-4 mt-8">
+          <RecentAppointments data={last5Records!} />
+        </div>
+      </div>
+
+      {/* the remaining 1% is for the gap */}
+      {/* RIGHT */}
+      <div className=" w-full xl:w-[30%]">
+        <div className="w-full h-[450px] mb-8">
+          <StatSummary data={appointmentCounts} total={totalAppointment!} />
+        </div>
+        <AvailableDoctors data={availableDoctors as any} />
+      </div>
+    </div>
+  );
+};
+
+export default DoctorDashboard;
--- utils/services/doctor.ts
+++ utils/services/doctor.ts
@@ -1,12 +1,99 @@
 import db from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { daysOfWeek } from "..";
+import { processAppointments } from "./patient";
 
 export async function getDoctors() {
-    try {
-        const data = await db.doctor.findMany()
+  try {
+    const data = await db.doctor.findMany();
 
-        return { success: true, message: "Doctor found", status: 200, data };
-    } catch (error) {
-        console.error(error);
-        return { success: false, message: "Internal server error", status: 500 };
-    }
+    return { success: true, message: "Doctor found", status: 200, data };
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: "Internal server error", status: 500 };
+  }
+}
+
+export async function getDoctorDashboardStats() {
+  try {
+    const { userId } = await auth();
+    const todayDate = new Date().getDay();
+    const today = daysOfWeek[todayDate];
+
+    // This code retrieves the total number of patients, nurses, appointments, and doctors
+    const [totalPatient, totalNurses, appointments, doctors] =
+      await Promise.all([
+        db.patient.count(),
+        db.staff.count({ where: { role: "NURSE" } }),
+        // Fetches all appointments for the current doctor (userId) that have already occurred or are currently in progress.
+        // This is done by checking if the appointment date is less than or equal to the current date.
+        db.appointment.findMany({
+          where: { doctor_id: userId!, appointment_date: { lte: new Date() } },
+          include: {
+            patient: {
+              select: {
+                id: true,
+                first_name: true,
+                last_name: true,
+                gender: true,
+                date_of_birth: true,
+                phone: true,
+                email: true,
+                colorCode: true,
+                img: true,
+              },
+            },
+            doctor: {
+              select: {
+                id: true,
+                name: true,
+                specialization: true,
+                img: true,
+                colorCode: true,
+              },
+            },
+          },
+          orderBy: {
+            appointment_date: "desc",
+          },
+        }),
+        db.doctor.findMany({
+          where: {
+            working_days: {
+              some: { day: { equals: today, mode: "insensitive" } },
+            },
+          },
+          select: {
+            id: true,
+            name: true,
+            specialization: true,
+            img: true,
+            colorCode: true,
+            working_days: true,
+          },
+          take: 5,
+        }),
+      ]);
+
+    const { appointmentCounts, monthlyData } = await processAppointments(
+      appointments
+    );
+
+    // 'last5Records' will be used to display the last 5 appointments in the dashboard because the appointments are ordered by date in descending order.
+    const last5Records = appointments.slice(0, 5);
+    // const availableDoctors = doctors.slice(0, 5);
+
+    return {
+      totalNurses,
+      totalPatient,
+      appointmentCounts,
+      last5Records,
+      availableDoctors: doctors,
+      totalAppointment: appointments?.length,
+      monthlyData,
+    };
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: "Internal server error", status: 500 };
+  }
 }
